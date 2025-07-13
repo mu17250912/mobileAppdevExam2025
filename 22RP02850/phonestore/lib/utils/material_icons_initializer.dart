4dad138905ace4536e1299eaa106d24c5dd138af
Yitakubayo <samuelyitakubayo70@gmail.com>
2025-07-13 12:20:11 +0200

    removing unwanted files

--- 22RP02850/phonestore/lib/utils/material_icons_initializer.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Initialize Material Icons
-Future<void> initializeMaterialIcons(BuildContext context) async {
-  // Initialize Material Icons font
-  await precacheImage(const AssetImage('packages/cupertino_icons/assets/CupertinoIcons.ttf'), context);
-}
